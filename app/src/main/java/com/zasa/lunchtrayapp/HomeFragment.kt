edfe52537efd2dfe3265560c58706737ca3d63a3
zansangeeth <zansangeeth17@gmail.com>
2022-06-15 01:25:30 +0530

    button onclick listener implemented

--- app/src/main/java/com/zasa/lunchtrayapp/HomeFragment.kt
+++ app/src/main/java/com/zasa/lunchtrayapp/HomeFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.zasa.lunchtrayapp.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    private lateinit var homeBinding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        homeBinding = FragmentHomeBinding.inflate(layoutInflater)
+        btnAdd.setOnClickListener {
+            Toast.makeText(this, "asdsd", Toast.LENGTH_LONG)
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
--- app/src/main/java/com/zasa/lunchtrayapp/MainActivity.kt
+++ app/src/main/java/com/zasa/lunchtrayapp/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.zasa.lunchtrayapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.zasa.lunchtrayapp.databinding.ActivityMainBinding
 
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController : NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
         setupWithNavController(binding.bottomNavigationView,navController)
 
     }
+
+
+
+
 }
